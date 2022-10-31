cd6276e0cc22ae597a2106444c9d148d7a9cea42
petersonprojetos <petersonrocha@gmail.com>
2022-10-31 13:04:53 -0300

    v1

---
+++ src/app/components/pages/tipousuario/novotipousuario/novotipousuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-novotipousuario',
+  templateUrl: './novotipousuario.component.html',
+  styleUrls: ['./novotipousuario.component.css']
+})
+export class NovotipousuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
